168194f5440759c0e02c77974d4fc125de24d8ef
nebelnest1 <nebelnest1488@gmail.com>
2026-01-29 12:01:47 +0300

    Add files via upload

---
+++ survey.js
@@ -0,0 +1,25 @@
+window.SURVEY_JS = [
+  {
+    "title": "attention",
+    "question": "intro_hard",
+    "answers": [
+      { "text": "no_18", "exit": "ageExit" }, 
+      { "text": "yes_18", "exit": "nextStep" }
+    ]
+  },
+  {
+    "title": "question",
+    "question": "pref_q",
+    "answers": [
+      { "text": "pref_1", "exit": "nextStep" },
+      { "text": "pref_2", "exit": "nextStep" }
+    ]
+  },
+  {
+    "title": "congratulations",
+    "question": "final_q",
+    "answers": [
+      { "text": "btn_watch", "exit": "mainExit" }
+    ]
+  }
+];
